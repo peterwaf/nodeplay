00a05f55de78b8de3bfa215c959d369bbdca9b48
peterwaf <peterwafulah@gmail.com>
2024-09-24 19:10:56 +0300

    add test post data

--- excercises/ex2/index.js
+++ excercises/ex2/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import bodyParser from "body-parser";
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(__dirname);
+
+app.use(bodyParser.urlencoded({ extended: true })); //should be below app
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/public/pages/home.html');
@@ -12,6 +14,13 @@ app.get('/',(req,res)=>{
 
 app.get('/about',(req,res)=>{
     res.sendFile(__dirname + '/public/pages/about.html');
+})
+
+app.post('/submit',(req,res)=>{
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const fullName = firstName + ' ' + lastName;
+    res.send('Form Submitted the following name is ' + fullName);
 })
 
 app.get('/css/style.css',(req,res)=>{
